5b8e60c8157ac7e80a6d00c514143ce418447110
ines.hnrqs <ines.hnrqs@gmail.com>
2019-10-02 14:04:40 +0100

    add install.R

---
+++ install.R
@@ -0,0 +1,9 @@
+install.packages("https://cran.r-project.org/src/contrib/foreach_1.4.7.tar.gz", repos=NULL, method="libcurl")
+install.packages("https://cran.r-project.org/src/contrib/iterators_1.0.12.tar.gz", repos=NULL, method="libcurl")
+install.packages("https://cran.r-project.org/src/contrib/doParallel_1.0.15.tar.gz", repos=NULL, method="libcurl")
+install.packages("https://cran.r-project.org/src/contrib/data.table_1.12.2.tar.gz", repos=NULL, method="libcurl")
+
+install.packages("https://cran.r-project.org/src/contrib/FNN_1.1.3.tar.gz", repos=NULL, method="libcurl")
+install.packages("https://cran.r-project.org/src/contrib/readr_1.3.1.tar.gz", repos=NULL, method="libcurl")
+install.packages("readr") 
+
